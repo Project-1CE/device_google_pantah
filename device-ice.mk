cf9e7ded6ab69a9a83b6983a2b62c3a1cae39c20
Anay Wadhera <anay1018@gmail.com>
2022-10-31 21:01:29 -0400

    pantah: Enable traffic light face support
    Change-Id: I35f5581a2efe7f9dbe9161f6a738d3206db4305b

--- device-ice.mk
+++ device-ice.mk
@@ -24,6 +24,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
 
+# Face unlock
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.biometrics.face.xml
+
 # IWLAN
 PRODUCT_PACKAGES += \
     Iwlan
