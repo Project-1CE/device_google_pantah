d990a662275e8cab13d025fd45271e3a7cbe990f
alk3pInjection <webmaster@raspii.tech>
2023-08-16 15:56:54 +0800

    pantah: Import stock CarrierConfig
    Extracted from lynx UP1A.231105.003
    
    Change-Id: I45a0b8315bbb851a736f1a468abdc473bdb46970

--- device-ice.mk
+++ device-ice.mk
@@ -9,6 +9,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-ice \
     $(LOCAL_PATH)/$(TARGET_DEVICE)/overlay-ice
 
+PRODUCT_PACKAGES += \
+    CarrierSettingsPantah
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
