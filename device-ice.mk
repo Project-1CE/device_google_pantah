61cd41cae99969ee766e904d78c20883552dcc20
alk3pInjection <webmaster@raspii.tech>
2023-08-16 15:56:54 +0800

    pantah: Import stock CarrierConfig
    Extracted from lynx BP2A.250805.005
    
    Change-Id: I45a0b8315bbb851a736f1a468abdc473bdb46970

--- device-ice.mk
+++ device-ice.mk
@@ -13,6 +13,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     ANGLE
 
+# CarrierConfig
+PRODUCT_PACKAGES += \
+    CarrierSettingsPantah
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
