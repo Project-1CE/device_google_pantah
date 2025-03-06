ef0380254292c3bd42b2be2c5dfbe606b72b5de8
Michael Bestas <mkbestas@gmail.com>
2025-03-06 23:31:19 +0200

    pantah: Include missing vibrator sepolicy
    Change-Id: Iac3a36e884dc1747d4fd0a60b418ffbf3779ffdb

--- sepolicy/cheetah-sepolicy.mk
+++ sepolicy/cheetah-sepolicy.mk
@@ -1,3 +1,5 @@
 # sepolicy that are shared among devices using whitechapel
 BOARD_SEPOLICY_DIRS += device/google/pantah/sepolicy/cheetah
 
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/common
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/cs40l26
